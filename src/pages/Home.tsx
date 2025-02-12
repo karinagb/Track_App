13e568fee4324b448f371c8ad6ced76ab2932c30
karinagb <karin.gb27@gmail.com>
2025-02-11 23:46:37 -0600

    home search button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,21 @@
 import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen bg-black text-white">
       <Header />
-      <div className="flex flex-col items-center justify-center h-[80vh] text-center">
+      <div className="flex flex-col space-y-6 items-center justify-center h-[80vh] text-center">
         <h1 className="text-5xl font-extrabold text-green-500">Track App</h1>
         <p className="text-gray-300 mt-3 text-lg max-w-md">
           Search for your favorite tracks by ISRC code and get instant details.
         </p>
+        <button
+          onClick={() => navigate("/search")}
+          className="inline-flex whitespace-nowrap px-4 py-3 bg-green-500 text-black font-semibold rounded-lg hover:bg-green-400 transition">
+          Search Track
+        </button>
       </div>
     </div>
   );
