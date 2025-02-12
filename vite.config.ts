2d3923d2fe47c20d81c5c9905bfea95959649ada
karinagb <karin.gb27@gmail.com>
2025-02-11 22:11:24 -0600

    deploy package.json change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/Track_App/',
   plugins: [react(), tailwindcss()],
 });
