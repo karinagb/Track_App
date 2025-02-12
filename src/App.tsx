ae12f9d0e437cde789c6a410da8ebdfeb591efc4
karinagb <karin.gb27@gmail.com>
2025-02-11 22:51:46 -0600

    add basename to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Search from './pages/Search'
-import Add from './pages/Add'
-
+import Search from "./pages/Search";
+import Add from "./pages/Add";
 
 import { Navigate, Outlet } from "react-router-dom";
 
@@ -20,7 +19,7 @@ const App = () => {
   const isAuthenticated = !!localStorage.getItem("accessToken");
 
   return (
-    <Router>
+    <Router basename="/track_app/">
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
